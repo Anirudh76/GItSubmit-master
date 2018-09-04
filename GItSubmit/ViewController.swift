cef5a0bc2f8b146f5a73f3cbcf91402ea156f2df
brn.developers <brn.developers@achiever-3.local>
2018-09-04 14:29:01 +0530

    Initial Commit

---
+++ GItSubmit/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  GItSubmit
+//
+//  Created by Azamuddin on 9/4/18.
+//  Copyright © 2018 Azamuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Azam")
+        
+        
+        print("This is Anirudh")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
